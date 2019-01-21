7286fe8fe7cdd5d9928d1e9bf8397fdb963d9a30
Christian <c.atallah2@newcastle.ac.uk>
2019-01-21 12:09:08 +0000

    x number of components are looked at, and calculate edgeless nodes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import networkx as nx
 from sifToNX import sifToNX
 from gini_simpson import gini_simpson_dict, gini_simpson_value
-from random import choice
+from random import choice, sample
 
 def uniprot_EC_dict(tab_file):
 
@@ -22,14 +22,13 @@ def uniprot_EC_dict(tab_file):
 
     return temp_dict
 
-def sample_from_components(components):
-    # return [choice(comp) for comp in components]
+def sample_from_components(components, num_comp):
 
     temp_list = []
     # print components
-    for comp in components:
+    for i in range(0, num_comp):
 
-            # print comp
+            comp = sample(components, 1)[0]
             temp_entry = choice(comp)
             # print temp_entry
             temp_list += [temp_entry]
@@ -42,11 +41,13 @@ def multi_ginisimpson(iterations, components, dict):
     avg_coverage = 0.0
     avg_value = 0.0
     min = 100.0
+    ec_num = len(set(dict.values()))*2/3
+    # ec_num = 50
 
     # print components
     for i in range(0, iterations):
 
-        rand_sample = sample_from_components(components)
+        rand_sample = sample_from_components(components, ec_num)
         ginisimps_dict = gini_simpson_dict(rand_sample, dict)
         ginisimps_value = gini_simpson_value(ginisimps_dict)
         avg_coverage += len(ginisimps_dict.keys())
@@ -64,6 +65,18 @@ def multi_ginisimpson(iterations, components, dict):
     avg_value /= iterations
     return avg_coverage, avg_value
 
+
+def get_edgeless_nodes(components):
+
+    count = 0
+
+    for comp in components:
+
+        if len(comp) == 1:
+            count += 1
+
+    return count
+
 def main():
 
     ssn_file = './examples/ssn_transIII_n50.gml'
@@ -72,14 +85,15 @@ def main():
     # csn_file = './examples/csn_SDR_n57.sif'
 
 
-    tab_file = './transaminases.tab'
-    # tab_file = './STR_143_annotation.tab'
+    tab_file = './transaminases_edited.tab.csv'
+    # tab_file = './STR_143_annotation_edited.csv'
 
     # ssn_graph = sifToNX(ssn_file)
     ssn_graph = nx.read_gml(ssn_file)
     csn_graph = sifToNX(csn_file)
 
     uniprot_EC = uniprot_EC_dict(tab_file)
+    ec_num = len(set(uniprot_EC.values()))
     iterations = 100000
 
     print "Calculating SSN Component Count.."
@@ -91,6 +105,16 @@ def main():
     csn_components = list(nx.connected_components(csn_graph))
     csn_comp_num = nx.number_connected_components(csn_graph)
     print "CSN Component Count: " + str(csn_comp_num) + '\n'
+
+    print "Calculating SSN Edgeless Nodes Count.."
+    ssn_edgeless = get_edgeless_nodes(ssn_components)
+    print "SSN Edgeless Nodes Count: " + str(ssn_edgeless) + '\n'
+
+    print "Calculating CSN Edgeless Nodes Count.."
+    csn_edgeless = get_edgeless_nodes(csn_components)
+    print "CSN Edgeless Nodes Count: " + str(csn_edgeless) + '\n'
+
+
 
 
     print "Calculating SSN Average EC Coverage and Gini-Simpson Index for " \
@@ -113,7 +137,7 @@ def main():
     print "CSN Average Gini-Simpson Index: " + str(csn_avg_ginisimpson) + "\n"
 
 
-    print "Total EC Numbers: " + str(len(set(uniprot_EC.values())))
+    print "Total EC Numbers: " + str(ec_num)
 
 
 if __name__ == '__main__':
